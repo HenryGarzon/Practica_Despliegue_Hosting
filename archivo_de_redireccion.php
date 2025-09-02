b6b5e8e9ba7abce4c79e7483c86740ae4857a351
Henry Alfonso Garzón Sánchez <halgarjr@gmail.com>
2025-09-02 04:30:02 -0500

    se deja archivo de redireccionamiento en php en la raíz para ser utilizado en la práctica de despliegue (NO CI/CD)

---
+++ archivo_de_redireccion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0; url=Practica_Despliegue_Hosting/homepage.html"> <!-- url de la carpeta del proyecto -->
+  <title>Redirigiendo...</title>
+</head>
+<body>
+  <p>Redirigiendo a la página principal... <br>
+     Si no eres redirigido automáticamente, haz clic aquí: 
+     <a href="Practica_Despliegue_Hosting/homepage.html">Ir a la página</a>
+  </p>
+</body>
+</html>
